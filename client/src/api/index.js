cc054784ee8a11173d2454ca7da09ce93bb3a639
feresOthman <othman.feres@gmail.com>
2021-09-24 16:20:11 +0200

    fix crud

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'http://localhost:5000' });
+const API = axios.create({ baseURL: 'http://fttt,herokuapp.com' });
 
 API.interceptors.request.use((req) => {
   if (localStorage.getItem('profile')) {
@@ -9,7 +9,7 @@ API.interceptors.request.use((req) => {
 
   return req;
 });
-export const Update = (formData,_id) => API.post(`/api/player/update/${_id}`, formData);
+export const Update = (formData, _id) => API.post(`/api/player/update/${_id}`, formData);
 export const Ajoutequipe = (formData) => API.post('/api/team/create', formData);
 
 export const Ajout = (formData) => API.post('/api/player/Ajoutjouer', formData);
@@ -17,4 +17,4 @@ export const Delete = (id) => API.delete(`/api/player/delete/${id}`);
 export const Deleteteam = (id) => API.delete(`/api/team/delete/${id}`);
 
 
-export const Updateequipe = (formData,_id) => API.post(`/api/team/update/${_id}`, formData);
+export const Updateequipe = (formData, _id) => API.post(`/api/team/update/${_id}`, formData);
